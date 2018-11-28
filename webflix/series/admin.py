edf165258914c73f40ee26c6ceffbf833534e2af
Renato <renatocrobledo@gmail.com>
2018-11-27 21:39:15 -0600

    wip

---
+++ webflix/series/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Serie, UserProfile
+
+admin.site.register(Serie)
+admin.site.register(UserProfile)
---
+++ webflix/series/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Serie(models.Model):
+
+    CATEGORIES_CHOICES = (
+        ('horror', 'Horror'),
+        ('comedy', 'Comedy'),
+        ('action', 'Action'),
+        ('drama', 'Drama'),
+    )
+
+    name = models.CharField(max_length=100)
+    release_date = models.DateField()
+    rating = models.IntegerField(default=0)
+    category = models.CharField(max_length=10, choices=CATEGORIES_CHOICES)
+
+    def __str__(self):
+      return self.name
+
+class UserProfile(models.Model):
+  user = models.OneToOneField(User, related_name="profile", on_delete=models.DO_NOTHING)
+  role = models.CharField(max_length=50)
